036f442b21cd04fabab02698cbffb575c499cec6
Tsyren Nimaev <cnimaev@gmail.com>
2026-02-05 22:02:42 +0300

    feat: init FSD structure with app, shared, entities, features layers

---
+++ src/app/App.tsx
@@ -0,0 +1,12 @@
+import './styles/global.scss';
+
+function App() {
+  return (
+    <div className='app'>
+      <h1>Finance Tracker PWA</h1>
+      <p> Стартовая страница.</p>
+    </div>
+  );
+}
+
+export default App;
