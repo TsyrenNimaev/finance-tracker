6c8e097d5663e601d2c35eec642cdc33295a76da
Tsyren Nimaev <cnimaev@gmail.com>
2026-02-10 14:40:03 +0300

    feat: set up RTK and configure store with typed hooks

---
+++ src/app/store/rootReducers.ts
@@ -0,0 +1,11 @@
+import { combineReducers } from '@reduxjs/toolkit';
+import transactionReducer from '../../entities/transaction/model/slice';
+import categoriesReducer from '../../entities/category/model/slice';
+
+const rootReducer = combineReducers({
+  trasaction: transactionReducer,
+  categories: categoriesReducer,
+});
+
+export type RootState = ReturnType<typeof rootReducer>;
+export default rootReducer;
